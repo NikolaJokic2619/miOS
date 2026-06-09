306e922aaf7d60373e3844630075d8f0759e1b6d
Nikola Jokic <183821966+NikolaJokic2619@users.noreply.github.com>
2026-06-09 20:08:17 +0200

    Linker added and backspace functionality extended

--- drivers/screen.c
+++ drivers/screen.c
@@ -92,18 +92,36 @@ void print_char(char character, int col, int row, char attribute_byte)
         offset = get_cursor();
     }
     
+    
+    
     if(character == '\n')
     {
         int rows = offset / (2 * MAX_COLS);
         offset = get_screen_offset(MAX_COLS - 1, rows);
     }
     else if(character == '\b')
-    {
+    {   
         if(offset > 0)
-        {
+        {   
             offset -= 2;
-            vidmem[offset] = ' ';
-        }    
+            
+            if(vidmem[offset] == ' ')
+            {
+                int i = 1;
+                while(offset > 0 && vidmem[offset] == ' ' && i < 80)
+                {
+                    offset -= 2;
+                    i++;
+                    
+                }
+            
+                if(vidmem[offset] != ' ')
+                    offset += 2;
+            }
+            else
+                vidmem[offset] = ' ';
+        }
+        
         offset -= 2;
         
     }
--- kernel/entry.asm
+++ kernel/entry.asm
@@ -3,6 +3,8 @@
 global _start
 extern kernel_main
 
+section .text.entry
+
 _start:
     mov esp, 0x90000
     call kernel_main
